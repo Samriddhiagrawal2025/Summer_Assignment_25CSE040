5311dc98f716efaebca06954baf2554bae1e64b9
Samriddhi Agrawal <samriddhi.agrawal2025@glbajajgroup.org>
2026-07-08 20:38:54 +0530

    Day-7 uploaded

---
+++ Day-7/recur_fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Recursive function to find nth Fibonacci number
+int fibonacci(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci number at position %d = %d\n", n, fibonacci(n));
+
+    return 0;
+}
