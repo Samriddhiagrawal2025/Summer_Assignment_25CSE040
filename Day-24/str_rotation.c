66171927ae9d39307a4358adab9db0201c093dfa
Samriddhi Agrawal <samriddhi.agrawal2025@glbajajgroup.org>
2026-07-09 00:50:13 +0530

    Day-24 uploaded

---
+++ Day-24/str_rotation.c
@@ -0,0 +1,30 @@
+ #include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100], temp[200];
+
+    printf("Enter first string: ");
+    scanf("%99s", str1);
+
+    printf("Enter second string: ");
+    scanf("%99s", str2);
+
+    // Check if lengths are equal
+    if (strlen(str1) != strlen(str2)) {
+        printf("The strings are not rotations.\n");
+        return 0;
+    }
+
+    // Concatenate str1 with itself
+    strcpy(temp, str1);
+    strcat(temp, str1);
+
+    // Check if str2 is a substring of temp
+    if (strstr(temp, str2) != NULL)
+        printf("The strings are rotations of each other.\n");
+    else
+        printf("The strings are not rotations.\n");
+
+    return 0;
+}
