dcaa546f9f3cf05dae21d5d887d5320d68667163
Samriddhi Agrawal <samriddhi.agrawal2025@glbajajgroup.org>
2026-07-08 21:39:59 +0530

    Day-15 uploaded

---
+++ Day-15/moves0toend.c
@@ -0,0 +1,32 @@
+
+  #include <stdio.h>
+
+void moveZerosToEnd(int arr[], int n) {
+    int count = 0;  // Position for next non-zero element
+
+    // Move non-zero elements to the front
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            arr[count++] = arr[i];
+        }
+    }
+
+    // Fill remaining positions with zeros
+    while (count < n) {
+        arr[count++] = 0;
+    }
+}
+
+int main() {
+    int arr[] = {0, 1, 0, 3, 12, 0, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    moveZerosToEnd(arr, n);
+
+    printf("Array after moving zeros to end: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
